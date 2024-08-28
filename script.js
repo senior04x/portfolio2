f5585ad2614b69858417e76f33740ab7903a9529
senior04x <gccc406@gmail.com>
2024-08-28 16:54:59 +0500

    delete some code

--- script.js
+++ script.js
@@ -21,34 +21,3 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-const aboutContent = document.querySelector('#about');
-const projectContent = document.querySelector('#projects');
-
-function handleScroll() {
-    const contentPosition = aboutContent.getBoundingClientRect().top;
-    const screenPosition = window.innerHeight / 10;
-
-    if (contentPosition > screenPosition) {
-        aboutContent.classList.add('show');
-    } else {
-        aboutContent.classList.remove('show');
-    }
-}
-function handleScroll2() {
-    const projectPosition = projectContent.getBoundingClientRect().top;
-    const screenPosition = window.innerHeight / 10;
-
-    if (projectPosition > screenPosition) {
-        projectContent.classList.add('show');
-    } else {
-        projectContent.classList.remove('show')
-    }
-}
-
-window.addEventListener('scroll', handleScroll);
-
-window.addEventListener('load', handleScroll);
-
-window.addEventListener('scroll', handleScroll2);
-
-window.addEventListener('load', handleScroll2);
