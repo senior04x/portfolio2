204cf7156f0c46df8e2297da9f20e9d4d6e99e28
senior04x <gccc406@gmail.com>
2024-08-28 16:36:18 +0500

    add-js

--- script.js
+++ script.js
@@ -19,3 +19,36 @@ document.addEventListener('DOMContentLoaded', function() {
         navMenu.classList.toggle('active');
     });
 });
+
+
+const aboutContent = document.querySelector('#about');
+const projectContent = document.querySelector('#projects');
+
+function handleScroll() {
+    const contentPosition = aboutContent.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 5;
+
+    if (contentPosition > screenPosition) {
+        aboutContent.classList.add('show');
+    } else {
+        aboutContent.classList.remove('show');
+    }
+}
+function handleScroll2() {
+    const projectPosition = projectContent.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 10;
+
+    if (projectPosition > screenPosition) {
+        projectContent.classList.add('show');
+    } else {
+        projectContent.classList.remove('show')
+    }
+}
+
+window.addEventListener('scroll', handleScroll);
+
+window.addEventListener('load', handleScroll);
+
+window.addEventListener('scroll', handleScroll2);
+
+window.addEventListener('load', handleScroll2);
