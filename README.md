cd1334a6ed7fdf24bf4813ff01e0765c09e71eeb
senior04x <gccc406@gmail.com>
2024-08-31 16:03:38 +0500

    add-3-language

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # portfolio2
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NV6KWR0L5K"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NV6KWR0L5K');
+</script>
--- script.js
+++ script.js
@@ -20,21 +20,19 @@ document.addEventListener('DOMContentLoaded', function() {
         navMenu.classList.toggle('active');
         
         if (navMenu.classList.contains('active')) {
-            menuToggle.innerHTML = '&#10005;'; // X icon
+            menuToggle.innerHTML = '&#10005;';
         } else {
-            menuToggle.innerHTML = '&#9776;'; // Hamburger icon
+            menuToggle.innerHTML = '&#9776;'; 
         }
     });
 
-    // Close the menu when clicking outside of it
     document.addEventListener('click', function() {
         if (navMenu.classList.contains('active')) {
             navMenu.classList.remove('active');
-            menuToggle.innerHTML = '&#9776;'; // Hamburger icon
+            menuToggle.innerHTML = '&#9776;'; 
         }
     });
 
-    // Prevent the menu from closing when clicking inside of it
     navMenu.addEventListener('click', function(event) {
         event.stopPropagation();
     });
@@ -75,3 +73,44 @@ window.addEventListener('load', handleScroll);
 window.addEventListener('scroll', handleScroll2);
 
 window.addEventListener('load', handleScroll2);
+
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('language-switcher-button');
+    const options = document.getElementById('language-options');
+    const languageOptions = options.querySelectorAll('li');
+
+    button.addEventListener('click', () => {
+        if (options.classList.contains('show')) {
+            options.classList.remove('show');
+            setTimeout(() => {
+                options.style.opacity = '0';
+            }, 500); 
+        } else {
+            options.style.opacity = '1';
+            options.classList.add('show');
+        }
+        button.textContent = ""
+    });
+
+    languageOptions.forEach(option => {
+        option.addEventListener('click', () => {
+            const lang = option.getAttribute('data-value');
+            button.textContent = option.textContent.trim(); 
+            options.classList.remove('show');
+            translatePage(lang);
+        });
+    });
+
+    function translatePage(lang) {
+        const elementsToTranslate = document.querySelectorAll('[data-en]');
+        elementsToTranslate.forEach(el => {
+            el.textContent = el.getAttribute(`data-${lang}`);
+        });
+    }
+    translatePage('en');
+});
+
+
+
+
+
