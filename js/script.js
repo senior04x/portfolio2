5432d78806a6bd4070964a3a79fd6966c7a37927
senior04x <gccc406@gmail.com>
2024-09-06 01:27:20 +0500

    s

--- js/script.js
+++ js/script.js
@@ -1,15 +1,4 @@
-// document.querySelector('form').addEventListener('submit', function(event) {
-//     event.preventDefault();
 
-//     const name = document.querySelector('#name').value;
-//     const email = document.querySelector('#email').value;
-//     const message = document.querySelector('#message').value;
-
-//     // alert(`Thank you, ${name}! Your message has been sent.`);
-    
-    
-//     event.target.reset();
-// });
 const aboutContent = document.querySelector('.presentation-title');
 const projectContent = document.querySelector('.text-center');
 
